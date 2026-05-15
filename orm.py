bef879e240c26ced9a16f771aa9e6c47e29b6f8d
Matvey K. <matveykax@yandex.ru>
2026-05-15 21:29:09 +0400

    Update orm.py

--- orm.py
+++ orm.py
@@ -2,9 +2,7 @@ import json
 
 import requests
 def get_response(req, url):
-  response = requests.get(url + "/get?word=" + req).content
+  response = requests.get(url + "/get?word=" + req.lower()).content
   response = response.decode('utf-8')
   parsed = json.loads(response)
   return parsed["word"]
-
-print(get_response("черемша", 'http://127.0.0.1:6767'))
