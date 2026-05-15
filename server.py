2b44c2923918b607dd5d324edccc76f5022bcfed
Matvey K. <matveykax@yandex.ru>
2026-05-15 22:25:32 +0400

    Update server.py

--- server.py
+++ server.py
@@ -10,22 +10,27 @@ def get_test():
     connection = sqlite3.connect('Data.db')
     cursor = connection.cursor()
     word = request.args.get('word')
-    cursor.execute("SELECT * FROM Fignya_Melkaya where word = ?", (word,))
+    cursor.execute("SELECT id, word, meaning FROM Fignya_Melkaya where word = ?", (word,))
     all_rows = cursor.fetchall()
     if all_rows:
         cursor.execute("UPDATE Fignya_Melkaya SET count = count + 1 WHERE word = ?", (word,))
         connection.commit()
         connection.close()
+        print(all_rows)
         return jsonify({'word': all_rows[0][2]})
     else:
         gemini_response = gemini_manager.get_response(word)
         print(gemini_response, word)
-        if gemini_response != "no":
+        if gemini_response != "no" or not gemini_response:
             cursor.execute("INSERT INTO Fignya_Melkaya (word, meaning, count) VALUES (?, ?, ?)",
                            (word, gemini_response[4:], 1))
+            connection.commit()
+            connection.close()
+            return jsonify({gemini_response[4:]})
+
         connection.commit()
         connection.close()
-        return gemini_response[4:]
+        return jsonify({'word': "К сожалению я на такое не отвечу. Давай другое слово попробуем."})
 
 
 if __name__ == '__main__':
