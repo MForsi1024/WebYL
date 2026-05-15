a29be6bb6304c7159c8e56ade96e2884f535143f
Matvey K. <matveykax@yandex.ru>
2026-05-15 21:09:28 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, jsonify
-
+import sqlite3
+import gemini_manager
 app = Flask(__name__)
 
 @app.route('/get', methods=['GET'])
 def get_test():
+    connection = sqlite3.connect('Data.db')
+    cursor = connection.cursor()
     word = request.args.get('word')
-    return jsonify({'word': word})
+    cursor.execute("SELECT * FROM Fignya_Melkaya where word = ?", (word,))
+    all_rows = cursor.fetchall()
+    if all_rows:
+        return jsonify({'word': all_rows[0][2]})
+    else:
+        gemini_response = gemini_manager.get_response(word)
+        print(gemini_response, word)
+        if gemini_response != "no":
+            cursor.execute("INSERT INTO Fignya_Melkaya (word, meaning) VALUES (?, ?)", (word, gemini_response[4:]))
+        connection.commit()
+        return gemini_response[4:]
 
-
-@app.route('/post', methods=['POST'])
-def post_test():
-    data = request.get_json()
-    return jsonify({'user': data})
 
 if __name__ == '__main__':
     app.run(port=6767)
